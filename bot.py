f198d15d91f3dce710d69ed7849b4a42f0b3595f
orangeappleak <40416975+orangeappleak@users.noreply.github.com>
2018-06-21 17:30:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ async def on_message(msg):
     if msg.content.lower().startswith('you have to'):
         possibilities=['hey,i have my free will in this server and i cant be doing as you say okay','i have my freedom over here so........']
         await client.send_message(msg.channel,random.choice(possibilities))
-    chat_filter=['fuck','fuck off','dick','esketit','bitch']
+    chat_filter=['fuck','fuck off','dick','bitch']
     msd=msg.content.split(" ")
     for word in msd:
         if word.lower() in chat_filter:
@@ -41,9 +41,11 @@ async def on_message(msg):
 
 @client.command(pass_context=True)
 async def coinflip(ctx):
+    await client.send_typing(ctx.message.channel)
     await client.say('whats youre call?')
     reply=await client.wait_for_message(channel=ctx.message.channel,author=ctx.message.author)
     poss=['tails','heads']
+    await client.send_typing(ctx.message.channel)
     await client.say('okay flipping the coin')
     gif=discord.Embed(title='coinflip')
     url=['https://cdn.discordapp.com/attachments/455323478267133962/459233673510780938/Flipping-coin-animated.gif',
@@ -51,30 +53,38 @@ async def coinflip(ctx):
     gif.set_image(url=random.choice(url))
     await client.say(embed=gif)
     await asyncio.sleep(2.0)
+    await client.send_typing(ctx.message.channel)
     final=await client.say('\n\n{}'.format(random.choice(poss)))
     if reply.content==final.content:
+        await client.send_typing(ctx.message.channel)
         await client.say('looks like you won MR.{}'.format(ctx.message.author.mention))
     else:
-        await client.say('you lost it\ni am actually addicted towards winning you know')
+        await client.send_typing(ctx.message.channel)
+        await client.say('you lost it\ni am actually addicted towards winning{}'.format(ctx.message.author.mention))
 
 @client.command(pass_context=True)
 async def kiss(ctx,member:discord.Member):
     k=discord.Embed(description='hey {},{} gave you kiss'.format(member.mention,ctx.message.author.mention))
-    kisses=['https://media.giphy.com/media/v4JbTGe4KJjKo/giphy.gif','https://cdn.discordapp.com/attachments/455323478267133962/459237164916670464/tenor.gif']
+    kisses=['https://media.giphy.com/media/v4JbTGe4KJjKo/giphy.gif'
+    ,'https://cdn.discordapp.com/attachments/455323478267133962/459237164916670464/tenor.gif']
     k.set_image(url=random.choice(kisses))
+    await client.send_typing(ctx.message.channel)
     await client.say(embed=k)
 
 @client.command(pass_context=True)#admin
 async def admin(ctx):
     if 'admin' in (role.name for role in ctx.message.author.roles):
+        await client.send_typing(ctx.message.channel)
         await client.say('yes you are an admin <@%s>' % ctx.message.author.id)
     else:
+        await client.send_typing(ctx.message.channel)
         await client.say('sorry,but you are not an admin')
 
 @client.command(aliases=['hi','hello','wassup','hey'],
 pass_context=True)#greest a member back
 async def greets(ctx):
     greetings=['hey','wassup','hello there','salutations','greetings','well my creator has socialized me,\n so hello there','i love to greet people,wassup']
+    await client.send_typing(ctx.message.channel)
     await client.send_message(ctx.message.channel,random.choice(greetings) + "<@%s>" % ctx.message.author.id)
 
 @client.command(pass_context=True)#tells info of the member
@@ -86,66 +96,86 @@ async def info(ctx,user:discord.Member):
     embed.add_field(name='member of the server from:',value=user.joined_at,inline=False)
     embed.set_thumbnail(url=user.avatar_url)
     embed.set_footer(text='\nTHAT\'S ALL I CAN GATHER')
+    await client.send_typing(ctx.message.channel)
     await client.send_message(ctx.message.author,embed=embed)
 
 @client.command(pass_context=True)#deletes the messages
 async def delete_messages(ctx):
     if 'admin' in (role.name for role in ctx.message.author.roles):
+        await client.send_typing(ctx.message.channel)
         await client.say('gimme a sec')
         await client.purge_from(channel=ctx.message.channel,limit=1000)
+        await client.send_typing(ctx.message.channel)
         await client.say('there you go <@%s>' % ctx.message.author.id)
     else:
+        await client.send_typing(ctx.message.channel)
         await client.say('you have to be an admin in order to that')
 
 @client.command(pass_context=True)#kicks a member
 async def kick(ctx,user:discord.Member):
     if 'admin' in (role.name for role in ctx.message.author.roles):
+        await client.send_typing(ctx.message.channel)
         await client.say('as you say')
         try:
             await client.kick(user)
+            await client.send_typing(ctx.message.channel)
             await client.say('done')
         except:
+            await client.send_typing(ctx.message.channel)
             await client.say('sorry but its forbidden :sweat_smile:')
     else:
+        await client.send_typing(ctx.message.channel)
         await client.say('sorry,but are you an admin :sweat_smile:')
 
 @client.command(pass_context=True)#repeats the typed message
 async def say(ctx):
+    await client.send_typing(ctx.message.channel)
     await client.say('what do you want me to repeat(indicate me using a # and a space):smile:')
     def c(msg):
         return msg.content.startswith('#')
     msgs=await client.wait_for_message(check=c,channel=ctx.message.channel)
     slice=msgs.content.split(" ")
+    await client.send_typing(ctx.message.channel)
     await client.say('i am saying:```%s```' % (' '.join(slice[1:])))
 
 @client.command(pass_context=True)#creates a new channel
 async def create_channel(context):
     if 'admin' in (role.name for role in context.message.author.roles):
         try:
+            await client.send_typing(context.message.channel)
             await client.send_message(context.message.channel,'what do you want to name it??')
             name=await client.wait_for_message(channel=context.message.channel,author=context.message.author,timeout=30.0)
+            await client.send_typing(context.message.channel)
             await client.send_message(context.message.channel,'is it a voice channel or a text channel')
             confirmmsg=await client.wait_for_message(channel=context.message.channel,author=context.message.author,timeout=30.0)
             if (confirmmsg.content=='voice'):
                 await client.create_channel(context.message.server,name.content,type=discord.ChannelType.voice)
+                await client.send_typing(context.message.channel)
                 await client.say('the voice channel named {} has been created'.format(name.content))
             elif(confirmmsg.content=='text'):
                 await client.create_channel(context.message.server,name.content,type=discord.ChannelType.text)
+                await client.send_typing(context.message.channel)
                 await client.say('the text channel named {} has been created'.format(name.content))
             else:
+                await client.send_typing(context.message.channel)
                 await client.say('wrong')
         except:
+            await client.send_typing(context.message.channel)
             await client.say('you took too long try again')
     else:
+        await client.send_typing(context.message.channel)
         await client.say('sorry but you must be an admin in order to do that')
 
 @client.command(pass_context=True)#deltes a channel
 async def delete_channel(ctx,channel:discord.Channel):
     if 'admin' in (role.name for role in ctx.message.author.roles):
+        await client.send_typing(ctx.message.channel)
         await client.say('wait a sec')
         await client.delete_channel(channel)
+        await client.send_typing(ctx.message.channel)
         await client.say('the channel named #{} has been deleted'.format(channel.name))
     else:
+        await client.send_typing(ctx.message.channel)
         await client.say('you must be an admin in order to that')
 
 @client.command(pass_context=True)#edits an existing channel
@@ -186,6 +216,8 @@ async def help(ctx):
     embed.add_field(name='edit_channel [channel_name]->',value='it edits an existing channel',inline=False)
     embed.add_field(name='coinflip->',value='flips a coin',inline=False)
     embed.add_field(name='rules->',value='rules of the server',inline=False)
+    embed.add_field(name='kiss [user_name]->',value='kisses a member in the server',inline=False)
+    await client.send_typing(ctx.message.channel)
     await client.send_message(ctx.message.author,embed=embed)
 
 @client.command(pass_context=True)#rules
@@ -198,12 +230,8 @@ async def rules(ctx):
     embed.add_field(name='4.',value='if the bot dosent work properly just text in the general channel regarding the issue of the bot and the bot name also',inline=False)
     embed.add_field(name='5.',value='do not use any kind of language or abusive words that can harm others',inline=False)
     embed.add_field(name='NOTE',value='any violations of the above mentioned rule can lead to kicking or banning you from the server')
+    await client.send_typing(ctx.message.channel)
     await client.send_message(ctx.message.author,embed=embed)
-
-@client.command(pass_context=True)
-async def weather(ctx):
-    await client.send_message('gathering the weather info,please wait')
-    awa
 
 @client.event#greets a new member on join
 async def on_member_join(member:discord.Member):
