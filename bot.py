17000031528b3863afcce48478a794887f54f275
orangeappleak <40416975+orangeappleak@users.noreply.github.com>
2018-06-23 15:12:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,6 +37,9 @@ async def on_message(msg):
     if msg.content.lower().startswith('?discord.py-documentation'):
         embed=discord.Embed(description='API Reference--->\nhttp://discordpy.readthedocs.io/en/latest/api.html',colour=discord.Colour.blue())
         await client.send_message(msg.channel,embed=embed)
+    if msg.content.lower().startswith('?discord.py_rewrite_documentation'):
+        embed=discord.Embed(title='here you go\nhttps://discordpy.readthedocs.io/en/rewrite/api.html',description='heres the documentation for the discord.py rewrite')
+        await client.say(embed=embed
     await client.process_commands(msg)
 
 @client.command(pass_context=True)
