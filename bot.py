eba4c4e54ffbd26ae9ea5889faf8e3d55358f4c0
orangeappleak <40416975+orangeappleak@users.noreply.github.com>
2018-08-07 16:37:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -220,8 +220,8 @@ async def on_member_join(member:discord.Member):
 async def level(ctx):
     with open("users.json","r") as f:
         user_info=json.load(f)
-        level=user_info[ctx.message.author.id]['level']
-        exp=user_info[ctx.message.author.id]['expierience']
+    level=user_info[ctx.message.author.id]['level']
+    exp=user_info[ctx.message.author.id]['expierience']
     print(user.name)
     print(level)
     print(exp)
